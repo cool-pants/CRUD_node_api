66f7cecc646c3ef4fd70da38f19ae66dbc7e9651
Mainak Sengupta <muban.sg@gmail.com>
2021-05-04 15:44:16 +0530

    added bill create

--- functions/index.js
+++ functions/index.js
@@ -41,7 +41,7 @@ app.post("/api/create", (req, res) => {
 });
 
 
-//  Read
+//  Read all
 //  GET
 app.get("/api/read", (req, res) => {
   (async () => {
@@ -75,11 +75,151 @@ app.get("/api/read", (req, res) => {
   })();
 });
 
+//  Read one
+//  GET
+app.get("/api/read/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("product").doc(req.params.id);
+      const product = await document.get();
+      const response = product.data();
+
+      return res.status(200).send(response);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
 //  Update
 //  PUT
+app.put("/api/update/:id", (req, res) => {
+  (async ()=>{
+    try {
+      const document = db.collection("product").doc(req.params.id);
+
+      await document.update({
+        typeid: req.body.typeid,
+        unit: req.body.unit,
+        weight: req.body.weight,
+        available: req.body.available,
+        icon: req.body.icon,
+        machine: req.body.machine,
+        name: req.body.name,
+        price: req.body.price,
+      });
+
+      return res.status(200).send();
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
 
 //  Delete
 //  DELETE
+app.delete("/api/delete/:id", (req, res) => {
+  (async ()=>{
+    try {
+      const document = db.collection("product").doc(req.params.id);
+
+      await document.delete();
+
+      return res.status(200).send();
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+//  Create Bill
+//  POST
+app.post("/api/bill", (req, res) => {
+  (async ()=>{
+    try {
+      const val = Math.floor(10 + Math.random() * 90);
+      await db.collection("bill").doc("/" + req.body.id + "/").create({
+        name: req.body.name,
+        item_id: req.body.item_id,
+        bill_id: val+req.body.name,
+      });
+
+      return res.status(200).send();
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+
+//  Read Bills for current user with name
+//  GET
+app.get("/api/getBill/:name", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("bill").doc(req.params.name);
+      const bill = await document.get();
+      const response = bill.data();
+
+      return res.status(200).send(response);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+//  Read One Bill
+//  GET
+app.get("/api/getBill/:name/bill/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("bill").doc(req.params.id);
+      const bill = await document.get();
+      const response = bill.data();
+
+      return res.status(200).send(response);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+
+//  Update
+//  PUT
+app.put("/api/updateBill/:id", (req, res) => {
+  (async ()=>{
+    try {
+      const document = db.collection("bill").doc(req.params.id);
+
+      await document.update({
+        name: req.body.name,
+        item_id: req.body.item_id,
+      });
+
+      return res.status(200).send();
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+//  Delete
+//  DELETE
+app.delete("/api/billDelete/:id", (req, res) => {
+  (async ()=>{
+    try {
+      const document = db.collection("bill").doc(req.params.id);
+
+      await document.delete();
+
+      return res.status(200).send();
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
 
 //  Export api to firebase cloud functions
 exports.app = functions.https.onRequest(app);
